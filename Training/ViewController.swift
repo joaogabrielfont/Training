433687013632d462b256649e7cf55d81eae39884
João Gabriel <joao.conceicao@monitoratec.com.br>
2018-08-28 13:52:50 -0300

    My first commit

---
+++ Training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by João Gabriel  on 28/08/2018.
+//  Copyright © 2018 fontana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
